3eaad363dd8dcbc8f1b14aa7645f7ffe9e1fee88
tudor-stoica <31967471+tudor-stoica@users.noreply.github.com>
2018-02-23 09:51:10 -0500

    Add files via upload

---
+++ stoica-cli-markbook/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] names = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+        int[] students = new int[names.length];
+
+        
+
+    }
+}
