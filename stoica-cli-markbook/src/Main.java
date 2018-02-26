64646efb808b3c4da96dc7b622f38706640d3be2
tudor-stoica <31967471+tudor-stoica@users.noreply.github.com>
2018-02-26 09:45:21 -0500

    Add files via upload

--- stoica-cli-markbook/src/Main.java
+++ stoica-cli-markbook/src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
-        String[] names = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
-        int[] students = new int[names.length];
-
-        
-
+        //Call my missingAssignments method in the main method
     }
+
+    public static void missingAssignments (int[] marks, String[] names){
+        for (int i = 0; i< marks.length - 1; i++){
+            if (marks[i] == 0) {
+                System.out.println(names[i] + " is missing the assignment.");
+            }
+        }
+    }
+
 }
